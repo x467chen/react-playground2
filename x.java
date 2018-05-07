1e597dc2e71285cfc0768800911dfc2de5658741
Kate <x467chen@uwaterloo.com>
2018-05-07 14:36:34 -0400

    t

---
+++ x.java
@@ -0,0 +1,28 @@
+public class Solution {
+    /*
+     * @param gas: An array of integers
+     * @param cost: An array of integers
+     * @return: An integer
+     */
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        // write your code here
+if (gas==null|| cost==null||gas.length==0||cost.length==0||gas.length!=cost.length)
+         return -1;
+         
+        int sum = 0;  
+        int total = 0;  
+        int index = 0;  
+        for(int i = 0; i < gas.length; i++){  
+            sum += gas[i]-cost[i];  
+            total += gas[i]-cost[i];  
+            if(sum < 0){  
+                index=i+1; 
+                sum = 0;   
+            } 
+        }  
+        if(total<0)
+            return -1;  
+        else
+            return index;  
+    }
+}
